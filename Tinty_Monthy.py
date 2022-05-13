c6be95cc3adcea742bf86094c2740a5d61589d28
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-05-13 23:46:03 +0100

    Update Tinty_Monthy.py

--- Tinty_Monthy.py
+++ Tinty_Monthy.py
@@ -5,49 +5,27 @@ My Heart fill message to you! 💌
 Get on your IDE and enjoy the fun! 😊"""
 
 class Months:
-
-    def get_month(self, number):
-
+    def get_month(self, user_input):
         months = {'1':'January',
-
                   '2':'February',
-
                   '3':'March',
-
                   '4':'April',
-
                   '5':'May',
-
                   '6':'June',
-
                   '7':'July',
-
                   '8':'August',
-
                   '9':'September',
-
                   '10':'October',
-
                   '11':'November',
+                  '12':'December'}
+   
+    
+        for key in months.keys():
+            if user_input == key:
+                return f"""Welcome to {months.get(user_input)}! 🌹
+You are an amazing programmer, keep coding.✌️💯♥️"""
 
-                  '12':'December'
 
-                      }
-
-        for key in months():
-
-            if key == number:
-
-                return f"""Welcome to {months[key]}! 🌹
-
-You are an amazing programmer, keep coding✌️💯♥️
-
-Kindly follow my IG @mikkystechs...I've got some free packages for you on my bio."""
-
-         
-
-month = Months
-
-output = month.get_month(input('Choose month 1-12: '))
-
-print(output)
+month = Months()
+message = month.get_month(input('Choose month 1-12'))
+print(message)
